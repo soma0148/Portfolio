fb3672372d98156006ef01087ae9ee68907e6ea8
soma0148 <somapauldk@gmail.com>
2023-05-10 10:54:13 +0200

    changed html,css & js

---
+++ script.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Portfolio</title>
+  <style>
+    /* Your CSS styles here */
+  </style>
+</head>
+<body>
+  <!-- Your HTML code here -->
+
+  <script>
+    // Example JavaScript code
+    document.addEventListener("DOMContentLoaded", function() {
+      // Add your JavaScript code here
+      console.log("Portfolio website loaded!");
+    });
+  </script>
+</body>
+</html>
