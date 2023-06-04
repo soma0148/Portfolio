2d237eb05bd191ff3e81ada574a1e74b9bb03288
soma0148 <somapauldk@gmail.com>
2023-06-04 13:37:42 +0200

    js added

--- script.js
+++ script.js
@@ -14,6 +14,52 @@ function toggleMenu() {
   
   }
 btn.addEventListener("click", toggleMenu);
+
+// Get the button elements by their ids
+var btn1 = document.getElementById("btn_1");
+var webBtn = document.getElementById("web_btn");
+var uxBtn = document.getElementById("ux_btn_2");
+var animBtn = document.getElementById("anim_btn_3");
+var indholdBtn = document.getElementById("indhold_btn_4");
+var portfolioBtn = document.getElementById("portfolio_btn_5");
+var aboutBtn = document.getElementById("about_btn_6");
+
+// Add click event listeners to the buttons
+btn1.addEventListener("click", function() {
+  console.log("Button 1 clicked!");
+  // Additional code for Button 1
+});
+
+webBtn.addEventListener("click", function() {
+  console.log("Web Button clicked!");
+  // Additional code for Web Button
+});
+
+uxBtn.addEventListener("click", function() {
+  console.log("UX Button clicked!");
+  // Additional code for UX Button
+});
+
+animBtn.addEventListener("click", function() {
+  console.log("Animation Button clicked!");
+  // Additional code for Animation Button
+});
+
+indholdBtn.addEventListener("click", function() {
+  console.log("Indhold Button clicked!");
+  // Additional code for Indhold Button
+});
+
+portfolioBtn.addEventListener("click", function() {
+  console.log("Portfolio Button clicked!");
+  // Additional code for Portfolio Button
+});
+
+aboutBtn.addEventListener("click", function() {
+  console.log("About Button clicked!");
+  // Additional code for About Button
+});
+
   
 
 
