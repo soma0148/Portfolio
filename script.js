953548f0b8d151f5b680352aec6b391015d315a5
soma0148 <somapauldk@gmail.com>
2023-07-01 21:48:35 +0200

    extra coding is removed

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-window.addEventListener("load", showTitle);
 
-let btn = document.querySelector(".toggle-btn");
-let menu = document.querySelector(".main-menu");
-
-function toggleMenu() {
-  menu.classList.toggle("shown");
-  let menuShown = menu.classList.contains("shown");
-  if (menuShown) {
-    console.log("menu shown");
-    btn.textContent`="luk";
-  } else {
-    console.log("menu shown");
-    btn.textContent`="menu";
-  }
-  
-  }
   // Pause the audio when the page is unloaded or hidden
   window.addEventListener('beforeunload', function() {
     var audio = document.getElementById('myAudio');
